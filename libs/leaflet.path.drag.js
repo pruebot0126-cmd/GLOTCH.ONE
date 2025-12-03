8efc1c2a544d1847e52085b7810733d07f091dc4
pruebot0126-cmd <pruebabot0126@gmail.com>
2025-12-02 18:17:13 -0600

    Update leaflet.path.drag.js

--- libs/leaflet.path.drag.js
+++ libs/leaflet.path.drag.js
@@ -1,52 +1,44 @@
+// Leaflet.Path.Drag — Permite arrastrar polígonos
 L.Path.include({
-    _onDragStart() {
-        this._dragging = true;
-        this._map.dragging.disable();
-    },
-    _onDragEnd() {
-        this._dragging = false;
-        this._map.dragging.enable();
+    _initEvents: function () {
+        if (L.Path.Drag) {
+            this.dragging = new L.Path.Drag(this);
+            this.dragging.enable();
+        }
     }
 });
 
-L.Path.addInitHook(function () {
-    if (this.options.draggable) {
-        this.dragging = new L.Handler.PathDrag(this);
-        this.dragging.enable();
-    }
-});
-
-L.Handler.PathDrag = L.Handler.extend({
+L.Path.Drag = L.Handler.extend({
     initialize: function (path) {
         this._path = path;
     },
-
     addHooks: function () {
-        this._path.on('mousedown', this._onDragStart, this);
+        this._path.on('mousedown', this._onDown, this);
     },
-
     removeHooks: function () {
-        this._path.off('mousedown', this._onDragStart, this);
+        this._path.off('mousedown', this._onDown, this);
     },
+    _onDown: function (e) {
+        this._startPoint = e.containerPoint.clone();
+        this._path._map.on('mousemove', this._onMove, this);
+        this._path._map.on('mouseup', this._onUp, this);
+    },
+    _onMove: function (e) {
+        const dx = e.containerPoint.x - this._startPoint.x;
+        const dy = e.containerPoint.y - this._startPoint.y;
+        this._startPoint = e.containerPoint.clone();
 
-    _onDragStart: function (e) {
-        const path = this._path;
-        path._onDragStart();
-
-        const startPoint = e.layerPoint;
-
-        const onMove = (ev) => {
-            const offset = ev.layerPoint.subtract(startPoint);
-            path._transform(offset);
-        };
-
-        const onUp = () => {
-            path._onDragEnd();
-            path._map.off('mousemove', onMove);
-            path._map.off('mouseup', onUp);
-        };
-
-        path._map.on('mousemove', onMove);
-        path._map.on('mouseup', onUp);
+        this._path.eachLatLng(function (latlng) {
+            const point = this._path._map.latLngToLayerPoint(latlng);
+            const newPoint = L.point(point.x + dx, point.y + dy);
+            const newLatLng = this._path._map.layerPointToLatLng(newPoint);
+            latlng.lat = newLatLng.lat;
+            latlng.lng = newLatLng.lng;
+        });
+        this._path.redraw();
+    },
+    _onUp: function () {
+        this._path._map.off('mousemove', this._onMove, this);
+        this._path._map.off('mouseup', this._onUp, this);
     }
 });
